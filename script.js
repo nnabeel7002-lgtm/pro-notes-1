064fa7522892b9415bd7569307a62ac087b71875
Nabeeha Nabeel <nnabeel7002@gmail.com>
2025-12-26 17:53:10 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+function ToggleNav() {
+    
+    const nav = document.getElementById("myNav");
+    
+    nav.classList.toggle("open");
+    
+}
+
+
+function checkLogin() {
+    let roll = document.getElementById("roll").value.trim();
+
+    let prefix = "BAIF25M";
+
+    if (roll.startsWith(prefix)) {
+        let num = parseInt(roll.substring(7)); // get 001–063
+
+        if (num >= 1 && num <= 63) {
+            window.location.href="home.html";
+        } else {
+            alert("Invalid Roll Number");
+        }
+    } else {
+        alert("Invalid Roll Number Format");
+    }
+}
+
+
